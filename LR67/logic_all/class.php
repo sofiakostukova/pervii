ac36b7b10a9de7a6dd1e8c550ea8d31e8565693d
sofiakostukova <90374595+sofiakostukova@users.noreply.github.com>
2021-12-22 14:51:58 +0300

    Update class.php

--- LR67/logic_all/class.php
+++ LR67/logic_all/class.php
@@ -3,6 +3,7 @@ require_once ('db_connect.php');
 
 
 class clas{
+    
     private $connect;
 
 function  getconnectdb($db){
@@ -16,9 +17,13 @@ public function showData(){
         inner join avtors on books.id_avtors = avtors.id 
         where books.id";
 
-$q = $this->connect->prepare($query) or die("ОШИБКА ЧТЕНИЯ ЗАПИСЕЙ!");
+    
+$q = $this->connect->prepare($query) or die("Ошибка чтения записей");
+    
 $q->execute();
+    
 $r = $q->fetchALL(PDO::FETCH_NUM);
+    
 return $r;
 }
 
@@ -30,13 +35,15 @@ public function getById($id){
         where books.id = :id";
 
 $q = $this->connect->prepare($query);
+    
 $q->execute(array(':id'=>$id));
+    
 $data = $q->fetchALL(PDO::FETCH_NUM);
 
 if($data){
     return $data;}
 else{
-    return "Произошла ошибка чтения по id";}
+    return "Ошибка чтения по id";}
 }
 
 
@@ -44,9 +51,13 @@ else{
 public function update($id, $img_name, $namebook, $avtor, $annotationbook, $pricebook, $file){
 
 $sql = "UPDATE books SET img=:imgnew, name=:namebook, id_avtors=:avtor, description=:annotationbook, cost=:pricebook where books.id = $id";
+    
 $q = $this->connect->prepare($sql);
+    
 unlink($_SERVER['DOCUMENT_ROOT']. "/img/" . $img_name);
+    
 $imgnew = $this->uploadfile($file);
+    
 $q->execute(array(':imgnew'=>$imgnew,':namebook'=>$namebook, ':avtor'=>$avtor,':annotationbook'=>$annotationbook, ':pricebook'=>$pricebook));
 
 //var_dump($img_name);
@@ -63,9 +74,13 @@ public function insert($namebook, $avtor, $annotationbook, $pricebook, $file)
 {
 
     $sql = "INSERT INTO books SET img=:imgnew, name=:namebook, id_avtors=:avtor, description=:annotationbook, cost=:pricebook";
+    
     $q = $this->connect->prepare($sql);
+    
     $imgnew = $this->uploadfile($file);
+   
     $q->execute(array(':imgnew'=>$imgnew,':namebook'=>$namebook, ':avtor'=>$avtor,':annotationbook'=>$annotationbook, ':pricebook'=>$pricebook));
+    
     if ($q) {
         header('location: ../table.php');
     } else {
@@ -76,11 +91,14 @@ public function insert($namebook, $avtor, $annotationbook, $pricebook, $file)
 
 public function delete($id, $img_name){
 $sql="DELETE FROM books WHERE id=:id";
+    
 $q = $this->connect->prepare($sql);
+    
 $q->execute(array(':id'=>$id));
+    
 unlink($_SERVER['DOCUMENT_ROOT']. "/img/" . $img_name);
 
-var_dump($img_name);
+//var_dump($img_name);
 
 if($q){
 header('location: ../table.php');}
@@ -94,8 +112,11 @@ else{
             return 'Выберете картинку';
         if($file['size'] == 0)
             return 'Картинка очень большая или отсутсвует';
+        
         $getMime = explode('.', $file['name']);
+        
         $mime = strtolower(end($getMime));
+        
         $types = array('jpg', 'png', 'gif', 'bmp', 'jpeg');
         if(!in_array($mime, $types))
             return 'Недопустимый тип файла';
@@ -107,18 +128,25 @@ else{
 
    public function uploadfile($file){
         $name = mt_rand(0, 999) . $file['name'];
+       
         copy($file['tmp_name'], 'img/' . $name);
+       
         return $name;
     }
+    
 public function getavtors(){
     $sql = "select * from avtors";
+    
     $avtors = $this->connect->prepare($sql);
+    
     $avtors->execute();
+    
     $row = $avtors->fetchALL(PDO::FETCH_ASSOC);
+    
     if($row){
    return $row;}
     else{
-        return "Таблица с местами погребения пуста!";
+        return "Таблица с авторами пуста";
     }
 }
 }
